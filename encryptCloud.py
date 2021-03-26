bd4bb4b196e474f28f52abf4b7ba3b49dbb4312e
oscarjim89 <oscar.jim.89@gmail.com>
2021-03-26 09:22:43 +0100

    Update encryptCloud.py

--- encryptCloud.py
+++ encryptCloud.py
@@ -3,7 +3,7 @@ import ibm_boto3
 from ibm_botocore.client import Config, ClientError
 
 COS_ENDPOINT = "https://s3.eu-de.cloud-object-storage.appdomain.cloud"
-COS_API_KEY_ID = "CimLFRbHslXgfCl3iQ4TsNnp4czIc24PKlWfsApX6-dF"
+COS_API_KEY_ID = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 COS_INSTANCE_CRN = "crn:v1:bluemix:public:cloud-object-storage:global:a/15f6dd4774d140888d8ec03dee3144e5:7999e5e0-9199-4382-859f-90763932fd17:bucket:cos.encrypt.test"
 cos = ibm_boto3.resource("s3",
     ibm_api_key_id=COS_API_KEY_ID,
