4769e1748480ff002df454963172be92fbc51213
oscarjim89 <oscar.jim.89@gmail.com>
2021-06-26 20:25:06 +0200

    Add files via upload

--- generateKey.py
+++ generateKey.py
@@ -1,11 +1,25 @@
 from cryptography.fernet import Fernet
+import sys, os
+import json
+import base64
 
-def write_key():
-    """
-    Generates a key and save it into a file
-    """
+CONFIG_PATH="OKV2cloud.conf"
+
+#Generates a key and save it into a file
+def write_key(key_name):
     key = Fernet.generate_key()
-    with open("key.key", "wb") as key_file:
+    with open(key_name, "wb") as key_file:
         key_file.write(key)
 
-write_key()
+if __name__ == "__main__":
+    #Load JSON config file with parameters
+    with open(CONFIG_PATH, 'rb') as config_file:
+	    #config = json.loads(base64.b64decode(config_file.read()).decode('utf8'))
+        config = json.loads(config_file.read())
+    #Check the path
+    if(os.path.isdir(config['key_path']) == False):
+        print("Object does not exist in specified path: "+os.path.realpath(config['key_path'])+" .The script will exit with error")
+        sys.exit(1)
+    #Generate new key
+    write_key(config['key_name'])
+    print("Output key: " + config['key_path'] + config['key_name'])
